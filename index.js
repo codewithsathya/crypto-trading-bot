daa3bb9b6a06ae5cf1dfb3c3a17f4ebebe4f46b4
S Sathiya <codewithsathya@gmail.com>
2021-08-09 15:55:15 +0530

    Simple bot

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { getLastCandles } = require("./services/getCandles");
-const { getMovingAvgValue } = require("./services/movingAverage");
+const { getMovingAvgValue, getMovingAvgArray } = require("./services/movingAverage");
 
 let now = new Date().getTime();
-let startTime = new Date("06/27/2021 09:10:00 PM").getTime();
+let startTime = new Date("07/13/2021 09:04:00 AM").getTime();
 let millis = startTime - now + 20;
 
 let minute = 60000;
 
-let sym = "QTUMUSDT";
+let sym = "AXSUSDT";
 console.log(sym);
 
 let principal = 1000;
@@ -21,7 +21,7 @@ setTimeout(() => {
 }, millis);
 
 function repeatEveryMinute() {
-  trade();
+  trade1();
   startTime += minute;
   let millis = startTime - new Date().getTime();
   setTimeout(repeatEveryMinute, millis);
@@ -31,8 +31,8 @@ async function trade() {
   let presentTime = new Date().getTime();
   const candles = await getLastCandles(sym, "1m", 100, presentTime);
 
-  movingAvg1 = getMovingAvgValue(candles, 14);
-  movingAvg2 = getMovingAvgValue(candles, 23);
+  movingAvg1 = getMovingAvgValue(candles, 44);
+  movingAvg2 = getMovingAvgValue(candles, 46);
 
   let price = parseFloat(candles[candles.length - 1][4]);
   if (movingAvg1 < movingAvg2) swit = 1;
@@ -48,6 +48,29 @@ async function trade() {
     usdt = 0.99925 * noOfCoins * price;
     count++;
     console.log("Sold & total =" + usdt + "count: " + count);
+    noOfCoins = 0;
+  }
+}
+
+async function trade1() {
+  let presentTime = new Date().getTime();
+  const candles = await getLastCandles(sym, "15m", 500, presentTime);
+
+  let movingAvg = getMovingAvgArray(candles, 10)
+  let price = parseFloat(candles[candles.length - 1][4]);
+  let len = movingAvg.length;
+
+  if(!bought && movingAvg[len - 1] > movingAvg[len - 2]){
+    bought = true;
+    noOfCoins = (0.999 * usdt) / price;
+    count++;
+    console.log("Bought & total: " + noOfCoins * price + " count: " + count);
+    usdt = 0;
+  }else if(bought && movingAvg[len - 1] < movingAvg[len - 2]){
+    bought = false;
+    usdt = 0.999 * noOfCoins * price;
+    count++;
+    console.log(`Sold & total = ${usdt} count: ${count}`);
     noOfCoins = 0;
   }
 }
